a37d5241611a719d61e8fcd5548a7296ca27b7b4
Kushagra <asthana.kushagra92@gmail.com>
2019-05-16 01:32:55 +0530

    Signed-off-by: Kushagra <asthana.kushagra92@gmail.com>

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const element = <h1> Your first react js page</h1>;
-
-console.log(element);
-
-ReactDOM.render(element, document.getElementById('root'));
