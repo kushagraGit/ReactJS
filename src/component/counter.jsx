bf80b689386648dd25082c921a7695c389e404b8
Kushagra <asthana.kushagra92@gmail.com>
2019-05-16 01:40:28 +0530

    First Commit
    Signed-off-by: Kushagra <asthana.kushagra92@gmail.com>

---
+++ src/component/counter.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1> Your first component! </h1>
+        <button>Test Button</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Counter;
