304e3e8f83f1486654215cda3917423e2646d657
Kushagra <asthana.kushagra92@gmail.com>
2019-05-15 23:30:02 +0530

    First Commit
    Signed-off-by: Kushagra <asthana.kushagra92@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1> Your first react js page</h1>;
+
+console.log(element);
+
+ReactDOM.render(element, document.getElementById('root'));
