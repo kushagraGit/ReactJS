01f604e6248c9cf0d6dcb6d3637b46e743f7abc0
Kushagra <asthana.kushagra92@gmail.com>
2019-05-17 23:50:26 +0530

    Setting Attributes - Part 1
    Signed-off-by: Kushagra <asthana.kushagra92@gmail.com>

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -3,7 +3,8 @@ import React, { Component } from "react";
 class Counter extends Component {
 
 state = {
-    count : 0
+    count : 0,
+    imageUrl: "https://picsum.photos/200"
 };
 
 formatCount(){
@@ -15,8 +16,9 @@ formatCount(){
     return (
       <React.Fragment>
         <h6> Your first component! </h6>
-        <span>{this.formatCount()}</span>
-        <button>Test Button</button>
+        <img src={this.state.imageUrl} alt="" />
+        <span className = "badge badge-primary m-2">{this.formatCount()}</span>
+        <button className = "btn btn-secondry btn-sm">Test Button</button>
       </React.Fragment>
     );
   }
