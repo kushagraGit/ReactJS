fa3ef8bf84974432f5c2f1da9457445903204dcd
Kushagra <asthana.kushagra92@gmail.com>
2019-05-17 23:30:47 +0530

    Embedding Expressions - Method 2
    Signed-off-by: Kushagra <asthana.kushagra92@gmail.com>

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -6,11 +6,16 @@ state = {
     count : 0
 };
 
+formatCount(){
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
+};
+
   render() {
     return (
       <React.Fragment>
         <h6> Your first component! </h6>
-        <span>{this.state.count}</span>
+        <span>{this.formatCount()}</span>
         <button>Test Button</button>
       </React.Fragment>
     );
