ff63e424fa64a9c3da37c803f40d35729cd724f0
Kushagra <asthana.kushagra92@gmail.com>
2019-05-18 15:06:14 +0530

    Updating State - Part 1
    Signed-off-by: Kushagra <asthana.kushagra92@gmail.com>

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -41,7 +41,8 @@ class Counter extends Component {
   }
 
   handleIncrement = () => {
-    console.log("Increment Clicked", this);
+    //console.log("Increment Clicked", this);
+    this.setState({count: this.state.count + 1 });
   }
 
   render() {
