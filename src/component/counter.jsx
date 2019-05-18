8367b016016612b86a3622a4c1300aca5b781eb8
Kushagra <asthana.kushagra92@gmail.com>
2019-05-18 15:00:30 +0530

    Binding Event Handlers - Method 2
    Signed-off-by: Kushagra <asthana.kushagra92@gmail.com>

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -1,51 +1,63 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-
-state = {
-    count : 0,
+  state = {
+    count: 0,
     imageUrl: "https://picsum.photos/200",
-    tags: ["tag1","tag2","tag3"]
-};
+    tags: ["tag1", "tag2", "tag3"]
+  };
 
-styles = {
-    fontSize : 10,
-    fontWeight : 'bold'
-};
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold"
+  };
 
-constructor(){
-    super();
-    this.handleIncrement = this.handleIncrement.bind(this);
-}
+//   constructor() {
+//     super();
+//     this.handleIncrement = this.handleIncrement.bind(this);
+//   }
 
-formatCount(){
+  formatCount() {
     const { count } = this.state;
     return count === 0 ? <h3>Zero</h3> : count;
-}
+  }
 
-newMethodBoot(){
+  newMethodBoot() {
     let classes = "badge m-2 badge-";
-    classes += (this.state.count === 0) ? "warning" : "primary";
+    classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
-}
+  }
 
-renderTags(){
-    if(this.state.tags.length === 0) return <p>Add some Tags</p>;
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>Add some Tags</p>;
 
-    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
-}
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
-handleIncrement(){
-    console.log("Increment Clicked",this);
-}
+  handleIncrement = () => {
+    console.log("Increment Clicked", this);
+  }
 
   render() {
     return (
       <React.Fragment>
         <h6> Your first component! </h6>
         <img src={this.state.imageUrl} alt="" />
-        <span className = {this.newMethodBoot()} /*style={this.styles}*/>{this.formatCount()}</span>
-        <button className = "btn btn-secondry btn-sm" onClick = {this.handleIncrement}>Increment</button>
+        <span className={this.newMethodBoot()} /*style={this.styles}*/>
+          {this.formatCount()}
+        </span>
+        <button
+          className="btn btn-secondry btn-sm"
+          onClick={this.handleIncrement}
+        >
+          Increment
+        </button>
         <div>{this.renderTags()}</div>
       </React.Fragment>
     );
